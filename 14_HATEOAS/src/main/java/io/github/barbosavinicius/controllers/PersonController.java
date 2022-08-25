2f64c667a239a73f7f3e5fdb4629985cc70ab7d0
Vinicius Barbosa <vinicius.silbarbosa@gmail.com>
2022-08-25 15:14:31 -0300

    API now works with HATEOAS. Created tests to PersonServices.

---
+++ 14_HATEOAS/src/main/java/io/github/barbosavinicius/controllers/PersonController.java
@@ -0,0 +1,51 @@
+package io.github.barbosavinicius.controllers;
+
+import io.github.barbosavinicius.data.vo.v1.PersonVO;
+import io.github.barbosavinicius.services.PersonServices;
+import io.github.barbosavinicius.util.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	//private PersonSerivces service = new PersonServices();
+
+	//@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/{id}",
+				produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+
+	@PostMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+				 consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	public PersonVO create(@RequestBody PersonVO personVO) {
+		return service.create(personVO);
+	}
+
+	@PutMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+				consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	public PersonVO update(@RequestBody PersonVO personVO) {
+		return service.update(personVO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		//return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ 14_HATEOAS/src/main/resources/db/migration/V2__Populate_Table_person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(3, 'Virgem da Lapa - MG - Brazil', 'Pedro', 'Male', 'Guimarães'),
+(5, 'Belo Horizonte - MG - Brazil', 'Francisca', 'Female', 'da Silva'),
+(6, 'Juiz de Fora - MG - Brazil', 'Fernanda', 'Female', 'Silva'),
+(7, 'Ouro Preto - MG - Brazil', 'Alipio', 'Male', 'Gonçalves'),
+(10, 'Unaí - MG - Brazil', 'Bárbara', 'Female', 'Almeira'),
+(11, 'Janaúba - MG - Brazil', 'Augusto', 'Male', 'da Costa');
